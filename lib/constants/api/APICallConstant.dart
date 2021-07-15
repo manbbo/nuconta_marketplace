f3317edc6881f3a6922f6b764faafc169a74997a
Andre Piton <andre.elias.vieira@gmail.com>
2021-07-15 20:03:47 -0300

    Delete APICallConstant.dart
    removing

--- lib/constants/api/APICallConstant.dart
+++
@@ -1,4 +0,0 @@
-class APICallConstant {
-  static const String API_URL = "https://staging-nu-needful-things.nubank.com.br/query";
-  static const String API_TOKEN = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJhd2Vzb21lY3VzdG9tZXJAZ21haWwuY29tIn0.cGT2KqtmT8KNIJhyww3T8fAzUsCD5_vxuHl5WbXtp8c";
-}
