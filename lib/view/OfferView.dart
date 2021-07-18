85ca4dc0b44da8c186cddfd2fd60b1fba5956793
Andre Piton <andre.elias.vieira@gmail.com>
2021-07-18 12:18:35 -0300

    Remaking the OffersListView.dart and creating the OfferView.dart

---
+++ lib/view/OfferView.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:nuconta_marketplace/model/ProductModel.dart';
+
+class OfferView extends StatelessWidget{
+  final Product product;
+  final Function onPressedFun;
+
+  OfferView ({this.product, this.onPressedFun});
+
+  Key getProductKey() {
+    return Key(product.id);
+  }
+
+  Widget view(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        margin: EdgeInsets.only(top: 50),
+        child: Directionality(
+          key: getProductKey(),
+          textDirection: TextDirection.ltr,
+          child: Column(
+            children: [
+              Container(
+                child: IconButton(
+                  icon: Icon(Icons.close),
+                  onPressed: () => Navigator.pop(context),
+                ),
+              ),
+              Container(
+                  child: Image.network(product.image)//image,
+              ),
+              Text(product.name),
+              Container(
+                height: 200,
+                child: SingleChildScrollView(
+                  child: Text(product.description),
+                ),
+              ),
+              Center(
+                child: FlatButton(
+                  child: Text("BUY"),
+                  onPressed: onPressedFun,
+                ),
+              )
+            ],),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return view(context);
+  }
+}
--- lib/view/OffersListView.dart
+++ lib/view/OffersListView.dart
@@ -1,43 +1,64 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:nuconta_marketplace/controller/UserBloc.dart';
 import 'package:nuconta_marketplace/model/OfferModel.dart';
+import 'package:nuconta_marketplace/view/OfferView.dart';
 
-class OfferListView {
+class OfferListView extends StatefulWidget {
   final List<OfferModel> offers;
+  final BuildContext context;
+  final UserBloc userBloc;
 
-  OfferListView ({this.offers});
+  OfferListView ({this.offers, this.context, this.userBloc});
 
-  Key getProductKey(int index) {
-    return Key(offers[index].product.id);
+  @override
+  _OfferListViewState createState() => _OfferListViewState();
+}
+
+class _OfferListViewState extends State<OfferListView> {
+  Key getOfferKey(int index) {
+    return Key(widget.offers[index].id);
   }
 
-  Widget listView(Function listener) {
+  Widget listView() {
     return Directionality(
       textDirection: TextDirection.ltr,
       child: ListView.builder(
-        itemCount: offers.length,
+        itemCount: widget.offers.length,
         itemBuilder: (BuildContext context, int index) {
-          var offer = offers[index];
-          var gKey = Key(offer.id);
+          var offer = widget.offers[index];
+          var gKey = getOfferKey(index);
           return GestureDetector(
             key: gKey,
-            onTap: listener,
+            onTap: () {
+              Navigator.push(context, new MaterialPageRoute(builder: (context) {
+                return new OfferView(product: offer.product, onPressedFun: (){
+                  if (this.widget.userBloc.subj.value.balance > 0) {
+                    setState(() {
+                      this.widget.userBloc.buy(offer.price);
+                    });
+                  }
+                },);
+              }));
+            },
             child: Container(
-              child: Row(
+              child: Column(
                 children: [
                   Container(
                       child: Image.network(offer.product.image)//image,
-                  ),Column(
-                    children: [
-                      Text(offer.product.name),
-                      Text(offer.product.description),
-                      Text("${offer.price}")
-                    ],
-                  )
+                  ),
+                  Text(offer.product.name),
+                  Text("Price: ${offer.price}")
                 ],),
             ),
           );
         },
       ),
     );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return listView();
   }
 }
